c1a3714a21c13cb44d294f7517b39751f609646c
2025234366-Xiao Zhixin <2024xxxxx@shanghaitech.edu.cn>
2026-01-01 16:37:20 +0800

    demo4.c rectified

--- projects/project/sw/src/demo4.c
+++ projects/project/sw/src/demo4.c
@@ -3,25 +3,31 @@
 
 int main() {
     int16_t a[8] = {1,2,3,4,5,6,7,8};
-    int16_t b[8] = {1,1,1,1,1,1,1,1};
-    int16_t c[16] = {0};
+    int16_t b[2][8] = {{1,1,1,1,1,1,1,1},
+                       {2,2,2,2,2,2,2,2}};
+    int16_t receive[32] = {0};
+    int16_t c[2] = {0};
 
     setvi16(); // e16
 
-    asm volatile("mv a0, %0" :: "r"(a));
-    asm volatile("mv a1, %0" :: "r"(b));
-    asm volatile("mv a2, %0" :: "r"(&c));
+    asm volatile("mv a1, %0" :: "r"(a));
+    asm volatile("mv a2, %0" :: "r"(&receive));
 
-    vle32_v1();
     vle32_v2();
-    vmul_vv();
-    vmv_v_x();
-    vredsum_vs();
-    vse32_v();
+    for(int i=0;i<2;i++){
+        asm volatile("mv a0, %0" :: "r"(b[i]));
+        vle32_v1();
+        vmul_vv();
+        vmv_v_x();
+        vredsum_vs();
+        vse32_v();
+        c[i]=receive[0];
+    }
+   
     printf("a addr = %p\n", a);
     printf("b addr = %p\n", b);
     printf("c addr = %p\n", &c);
-    for(int i = 0; i < 16; i++) {
+    for(int i = 0; i < 2; i++) {
         printf("c[%d] = %d\n", i, c[i]);
     }
 
